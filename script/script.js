b21eaa6b7382a85e3d5e10d5c7938df3fd2c9a16
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-10-07 10:35:39 +0800

    commit

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
-let info = "We couldn't verify your mother's maiden name.";
-let intro = 'Here is important information about your account security.';
+// let info = "We couldn't verify your mother's maiden name.";
+// let intro = 'Here is important information about your account security.';
 
-let firstName = 'Joffrey';
-let greeting = 'Hello';
-console.log(greeting +', ' + firstName + '!');
-console.log(intro + '\n' + info);
-// console.log(info);
+// let firstName = 'Joffrey';
+// let greeting = 'Hello';
+// console.log(greeting +', ' + firstName + '!');
+// console.log(intro + '\n' + info);
+// // console.log(info);
+const name = 'Na\nharis';
+console.log(name[name.length-1]);
+console.log(typeof name);
