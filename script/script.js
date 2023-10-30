e0ee2e23b0cbbd5e37931e2b907d468671d4e7a6
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-10-30 20:32:25 +0800

    project block layout

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
-const text = 'Never forget what you are, for surely the world will not';
-const result = `First: ${text[0]}\nLast: ${text[text.length -1]}`;
-console.log(result);
+// const text = 'Never forget what you are, for surely the world will not';
+// const result = `First: ${text[0]}\nLast: ${text[text.length -1]}`;
+// console.log(result);
+// const random = Math.random() * 10;
+// console.log(random);
+
+// const result = Math.round(random);
+// console.log(result);
+console.log(Math.round(Math.random() * 10));
