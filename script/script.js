985d8830c96b95f67f501075bf7761d5f5837121
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2023-11-05 14:15:03 +0800

    portfolio block layout

--- script/script.js
+++ script/script.js
@@ -6,4 +6,13 @@
 
 // const result = Math.round(random);
 // console.log(result);
-console.log(Math.round(Math.random() * 10));
+// console.log(Math.round(Math.random() * 10));
+// const text = 'Never forget what you are, for surely the world will not';
+// const result =`First: ${text[0]}\nLast: ${text[text.length - 1]}`;
+// console.log(`First: ${text[0]}\nLast: ${text[text.length - 1]}`);
+// const name = 'Hello hello zero';
+// console.log(name.toUpperCase().toLowerCase());
+// const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
+// console.log(text.slice(9, 15).trim().length);
+const name = 'Tirion';
+console.log(name.length.toString());
