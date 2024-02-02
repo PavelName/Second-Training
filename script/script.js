fc2e597346143c2339566b47b4fb30077bbf2fbb
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-02-02 20:04:20 +0800

    exsersizes in array

--- script/script.js
+++ script/script.js
@@ -184,11 +184,21 @@
 // };
 // console.log(`Чистая ЗП: ${calculateSalary(145000)}`);
 
-let count = [1, 3, 5, 6, 68];
-for(let i = 0; i <= count.length - 1; i++){
-  console.log(count[i]);
-}
-let count1 = [1, 3, 5, 6, 68];
-for(let i = count.length - 1; i > 0; i--){
-  console.log(count[i]);
-}
+// let count = [1, 3, 5, 6, 68, 5];
+// for(let i = 0; i <= count.length - 1; i++){
+//   console.log(count[i]);
+// }
+// let count1 = [1, 3, 5, 6, 68];
+// for(let i = count.length - 1; i > 0; i--){
+//   console.log(count[i]);
+// }
+let symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
+
+let encodedSymbols = [18, 38, 46, 62, 66, 50, 33, 41, 66, 49, 48, 38, 58, 62, 68, 66, 48, 37, 42, 47, 66, 50, 33, 41, 66, 49, 48, 51, 49, 42, 67];
+
+let decodedMasege = '';
+for(let i = 0; i <= encodedSymbols.length; i++){
+  const index = encodedSymbols[i];
+  decodedMasege += symbols[index];
+};
+console.log(decodedMasege);
