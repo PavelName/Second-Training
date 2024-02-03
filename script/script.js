57ec60644c1f96d8f94d1969f50ba1a4a00c7faa
Pavel Koryagin <pavel.koryagin.76@bk.ru>
2024-02-03 21:34:09 +0800

    changet array

--- script/script.js
+++ script/script.js
@@ -192,14 +192,22 @@
 // for(let i = count.length - 1; i > 0; i--){
 //   console.log(count[i]);
 // }
-let symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
+// let symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
 
-let encodedSymbols = [1, 38, 46, 62, 66, 50, 33, 41, 66, 49, 48, 38, 58, 62, 68, 66, 48, 37, 42, 47, 66, 50, 33, 41, 66, 49, 48, 51, 49, 42, 67];
+// let encodedSymbols = [18, 38, 46, 62, 66, 50, 33, 41, 66, 49, 48, 38, 58, 62, 68, 66, 48, 37, 42, 47, 66, 50, 33, 41, 66, 49, 48, 51, 49, 42, 67];
 
-let decodedMasege = '';
-for(let i = 0; i <= encodedSymbols.length; i++){
-  const index = encodedSymbols[i];
-  decodedMasege += symbols[index];
-};
-console.log(decodedMasege);
-console.log(symbols[1])
+// let decodedMasege = '';
+// for(let i = 0; i <= encodedSymbols.length; i++){
+//   const index = encodedSymbols[i];
+//   decodedMasege += symbols[index];
+// };
+// console.log(decodedMasege);
+// console.log(symbols[1]);
+
+let encodedSymbols = [2, 1, 3, 5];
+let swap = encodedSymbols[0];
+console.log(encodedSymbols);
+encodedSymbols[0] = encodedSymbols[1];
+console.log(encodedSymbols);
+encodedSymbols[1] = swap;
+console.log(encodedSymbols);
